03177633cfc252b25c005134343cd6c10bd16817
UR Raju <riadhasanraju>
2023-12-22 04:14:39 +0800

    delete api create with user data found done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrkwx23.mongodb.net/?retryWrites=true&w=majority`;
@@ -15,38 +15,64 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const  taskCollection = client.db("taskDB").collection("task");
-    
-    app.post('/task', async(req,res) => {
-      const query = req.body
-      const result = await taskCollection.insertOne(query)
-      res.send(result)
-    })
-    app.get('/task', async(req,res) => {
-      const result = await taskCollection.find().toArray()
-      res.send(result)
-    })
+    const taskCollection = client.db("taskDB").collection("task");
+
+    app.post("/task", async (req, res) => {
+      const query = req.body;
+      const result = await taskCollection.insertOne(query);
+      res.send(result);
+    });
+    app.get("/task", async (req, res) => {
+      const query = req.query?.email;
+      const filter = { userEmail: query };
+      const result = await taskCollection.find(filter).toArray();
+      res.send(result);
+    });
+    app.delete(
+      "/task/:id",
+
+      async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await taskCollection.deleteOne(query);
+        res.send(result);
+      }
+    );
+    app.put("/task/updateProduct/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+      const body = req.body;
+      const updatedDoc = {
+        $set: {
+           title: body.title,
+           priority: body.priority,
+          description: body.description,
+          deadline: body.deadline,
+          
+        },
+      };
+      const result = await taskCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-     
   }
 }
 run().catch(console.dir);
 
-
-
-
 app.get("/", async (req, res) => {
-    res.send("Server is Running");
-  });
-  app.listen(port, () => {
-    console.log("Server is Running on PORT ||", port);
-  });
-  
+  res.send("Server is Running");
+});
+app.listen(port, () => {
+  console.log("Server is Running on PORT ||", port);
+});
